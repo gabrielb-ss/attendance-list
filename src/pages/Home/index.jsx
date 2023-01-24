d578eefce42b1d77201a4992c3b76ef88c40b059
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-01-24 16:38:46 -0300

    add header and fetch header name and avatar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./styles.css"
 
 import { Card } from  "../../components/Card"
@@ -6,7 +6,8 @@ import { Card } from  "../../components/Card"
 export function Home() {
   const [studentName, setStudentName] = useState('');
   const [students, setStudents] = useState([]);
-
+  const [user, setUser] = useState({ name:'', avatar:''})
+  
   function handleAddStudent() {
     const newStudent = {
       name: studentName,
@@ -20,34 +21,51 @@ export function Home() {
     setStudents(prevState => [...prevState, newStudent]);
   }
   
+  useEffect(() => {
+    fetch("https://api.github.com/users/gabrielb-ss")
+    .then(response => response.json())
+    .then(data => {
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url
+      })
+    })
+  }, [])
+
   return (
-   <div className="container">
-    <h1>Lista de Presença</h1>
+    <div className="container">
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Profile Pic" />
+        </div>
+      </header>
 
-    <input 
-      type="text" 
-      placeholder="Digite seu nome" 
-      onChange={e => setStudentName(e.target.value)}
-    />
+      <input 
+        type="text" 
+        placeholder="Digite seu nome" 
+        onChange={e => setStudentName(e.target.value)}
+      />
 
-    <button 
-      type="button"
-      onClick={handleAddStudent}
-    >
-      Adicionar
-    </button>
+      <button 
+        type="button"
+        onClick={handleAddStudent}
+      >
+        Adicionar
+      </button>
 
-    {
-      students.map( student =>  (
-        <Card 
-          key={student.time}
-          name={student.name} 
-          time={student.time}
-        />
+      {
+        students.map( student =>  (
+          <Card 
+            key={student.time}
+            name={student.name} 
+            time={student.time}
+          />
+          )
         )
-      )
-    }
+      }
 
-   </div>
+    </div>
   )
 }
