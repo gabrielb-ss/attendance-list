b63d54078b2a6b4ae05bb6231ea6555b3576beb4
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-01-24 16:11:34 -0300

    add States do generate student cards from user input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,52 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 import "./styles.css"
 
 import { Card } from  "../../components/Card"
 
 export function Home() {
-  const [count, setCount] = useState(0)
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState([]);
 
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      })
+    }
+
+    setStudents(prevState => [...prevState, newStudent]);
+  }
+  
   return (
    <div className="container">
     <h1>Lista de Presença</h1>
-    <input type="text" placeholder="Digite seu nome" />
-    <button type="button">Adicionar</button>
 
-    <Card name="Gabriel" time="10:55:25"/>
-    <Card name="Sarah" time="08:55:25"/>
+    <input 
+      type="text" 
+      placeholder="Digite seu nome" 
+      onChange={e => setStudentName(e.target.value)}
+    />
+
+    <button 
+      type="button"
+      onClick={handleAddStudent}
+    >
+      Adicionar
+    </button>
+
+    {
+      students.map( student =>  (
+        <Card 
+          key={student.time}
+          name={student.name} 
+          time={student.time}
+        />
+        )
+      )
+    }
 
    </div>
   )
