f9b194e91116f2cc9a9783a8583fc0c4c2453db9
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-01-25 18:25:17 -0300

    change Home return to be a form
    Using <form/> simplifies the code and make input warnings better

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,23 +8,18 @@ export function Home() {
   const [students, setStudents] = useState([]);
   const [user, setUser] = useState({ name:'', avatar:''})
   
-  function handleAddStudent() {
-    if(studentName !== '' && !/[^a-zA-Z]/.test(studentName) && studentName.length >= 3){
-      const newStudent = {
-        name: studentName,
-        time: new Date().toLocaleTimeString("pt-br", {
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-        })
-      }
-      setStudents(prevState => [...prevState, newStudent]);
-      setStudentName('')
-    } else {
-      alert("Por favor insira um nome com pelo menos 3 letras e apenas letras")
-      setStudentName('')
-      console.log("deu ruim")
+  function handleAddStudent(event) {
+    event.preventDefault();
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      })
     }
+    setStudents(prevState => [...prevState, newStudent]);
+    setStudentName('')
   }
 
   useEffect(() => {
@@ -39,7 +34,7 @@ export function Home() {
   }, [])
 
   return (
-    <div className="container">
+    <form onSubmit={handleAddStudent} className="container">
       <header>
         <h1>Lista de Presença</h1>
         <div>
@@ -51,15 +46,16 @@ export function Home() {
       <input 
         id='input_name'
         type="text" 
-        value={studentName}
+        pattern="^[a-zA-Z, ]+$"
         minLength="3"
         placeholder="Digite seu nome" 
+        value={studentName}
         onChange={e => setStudentName(e.target.value)}
+        required
       />
 
       <button 
         type="submit"
-        onClick={handleAddStudent}
       >
         Adicionar
       </button>
@@ -75,6 +71,6 @@ export function Home() {
         )
       }
 
-    </div>
+    </form>
   )
 }
